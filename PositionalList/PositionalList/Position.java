8b66918bccb91c93e42048f9856b55f58946a206
Pahuldeep Singh <pahuldeepsingh12@gmail.com>
2024-08-29 07:44:01 -0600

    PositionalList

---
+++ PositionalList/PositionalList/Position.java
@@ -0,0 +1,14 @@
+/*
+Name Pahuldeep Singh
+ * Student 3153555
+ * From Data Structures and Algorithms in Java, Sixth Edition, Goodrich et al.
+ */
+public interface Position<E>{
+  /**
+   * Returns the element stored at this position.
+   *
+   * @return the stored element
+   * @throws IllegalStateException if position no longer valid 
+   */
+  E getElement() throws IllegalStateException;
+}
